1d7cbac37297be0848e9436c7171776cd4450ccb
Enrique Chenú <enchenu@gmail.com>
2017-10-20 16:33:20 -0300

    Adding Extra Field to Course Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019141645) do
+ActiveRecord::Schema.define(version: 20171020192444) do
 
   create_table "courses", force: :cascade do |t|
     t.string "image"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171019141645) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "public"
   end
 
 end
